12b08b36d6c62d5460d18fa83dd8f31f2e7f21ac
David Lee <davideuijinlee@gmail.com>
2022-02-27 12:07:57 -0800

    updated callback to promise syntax

--- index.js
+++ index.js
@@ -1,80 +1,62 @@
 #! /usr/bin/env node
 const fs = require("fs");
+const { performance } = require('perf_hooks');
 
 if (process.argv.length !== 5) {
-  console.log("Usage: buildbook <spotify.json> <changes.json> <output-fil.json>");
+  console.log("Usage: buildbook <spotify.json> <changes.json> <output-file.json>");
   process.exit(1);
 }
 
-const readJsonFile = (filePath, callback) => {
-  fs.readFile(filePath, (err, fileData) => {
-    if (err) {
-      return callback && callback(err);
+const readJsonFile = async (filePath) => {
+  return new Promise((resolve, reject) => {
+    if (!filePath) {
+      reject(Error("File not found"));
     }
-    try {
-      const object = JSON.parse(fileData);
-      return callback && callback(null, object);
-    } catch (err) {
-      return callback && callback(err);
-    }
-  });
+
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(JSON.parse(data));
+    });
+  })
 }
 
-const readJson = () => {
-  readJsonFile(`./${process.argv[2]}`, (err, data) => {
-    if (err) {
-      console.log("Error reading input file:", err);
-      return;
-    }
-    const inputJson = data;
+const readJson = async () => {
+  const inputJson = await readJsonFile(`./${process.argv[2]}`);
+  const editJson = await readJsonFile(`./${process.argv[3]}`);
 
-    readJsonFile(`./${process.argv[3]}`, (err, data) => {
-      if (err) {
-        console.log("Error reading edit file:", err);
-        return;
-      }
-      const editJson = data;
-
-      updateJson(inputJson, editJson)
-    })
-  })
+  updateJson(inputJson, editJson);
 }
 
 const updateJson = (input, edit) => {
   const keys = Object.keys(edit);
 
-  const containsValue = (json, value) => {
-    let contains = false;
-    Object.keys(json).some(key => {
-      contains = typeof json[key] === "object" ? containsValue(json[key], value) : json[key] === value;
-      return contains;
-    });
-    return contains;
-  }
-
   keys.forEach((key) => {
     switch (key) {
       case "add_song":
         for (const song of edit["add_song"]) {
-          if (containsValue(input, song["song_id"])) {
+          if (containsValue(input["songs"], song["song_id"])) {
             input["playlists"].find(obj => obj["id"] === song["playlist_id"])["song_ids"].push(song["song_id"])
           }
         }
         break;
       case "add_playlist":
         for (const playlist of edit["add_playlist"]) {
-          if (containsValue(input, playlist["owner_id"])) {
-            if (playlist["song_ids"].length >= 1) {
-              input["playlists"].push({
-                "id": `${input["playlists"].length + 1}`, "owner_id": playlist["owner_id"], "song_ids": playlist["song_ids"]
-              })
-            }
+          if (containsValue(input["users"], playlist["owner_id"]) && playlist["song_ids"].length >= 1) {
+            input["playlists"].push(
+              {
+                "id": `${input["playlists"].length + 1}`,
+                "owner_id": playlist["owner_id"],
+                "song_ids": playlist["song_ids"]
+              }
+            )
           }
         }
         break;
       case "remove_playlist":
         for (const playlist of edit["remove_playlist"]) {
-          if (containsValue(input, playlist["id"])) {
+          if (containsValue(input["playlists"], playlist["id"])) {
             input["playlists"].forEach((currentPlaylist, index) => {
               if (currentPlaylist["id"] === playlist["id"]) {
                 input["playlists"].splice(index, 1);
@@ -91,4 +73,17 @@ const updateJson = (input, edit) => {
   });
 }
 
+const containsValue = (json, value) => {
+  let contains = false;
+  Object.keys(json).some(key => {
+    contains = typeof json[key] === "object" ? containsValue(json[key], value) : json[key] === value;
+    return contains;
+  });
+  return contains;
+}
+
+let start = performance.now();
 readJson();
+let end = performance.now();
+
+console.log(`Execution time: ${end - start} ms`);
